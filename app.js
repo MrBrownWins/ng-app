7435d4f30b097d912a91f9cbc2f313370ea53a28
Mukhammad Karimov <karimov8887@gmail.com>
2017-02-18 22:25:07 +0500

    filters

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 var app = angular.module("AngularApp", []);
 
-app.controller("mainController", function($scope, $http){
+app.controller("mainController", function($scope, $timeout){
     $scope.name = "Mukhammad Karimov";
+    $scope.clock = {};
+    var updateClock = function() {
+        $scope.clock.now = new Date();
+        $timeout(function(){
+            updateClock();
+        }, 1000);
+    };
+    updateClock();
     console.log($scope);
-    console.log($http);
 });
